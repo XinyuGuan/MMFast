282e431e0def88fbe94203422af9ef7c209f45a6
xinyu23 <xinyu23@stanford.edu>
2025-11-05 07:57:12 -0800

    add a naive version of evaluation

--- first_mm.cpp
+++ first_mm.cpp
@@ -1,11 +1,8 @@
-
-
-
 #include <algorithm>
 #include <math.h>
 #include <stdio.h>
 #include <vector>
-
+#include <cassert>
 
 
 void build_matrix(float* matrix, int width, int height, float initial) {
@@ -18,22 +15,35 @@ void build_matrix(float* matrix, int width, int height, float initial) {
 void print_matrix(float* matrix, int width, int height) {
     printf("This is a %d * %d matrix. \n", height, width);
     printf("The matrix takes %d bytes. \n", sizeof(*matrix) * width * height);
+
     printf("\n");
+
     for (int h = 0; h < height; h++) {
         for (int w = 0; w < width; w++) {
-            printf("%f", *(matrix + h * width + w));
+	    printf("%f", *(matrix + h * width + w));
             if (w + 1 == width) {
-	         printf("\n");
+	        printf("\n");
 	    }       
 	    else {
-	         printf(", ");
+	        printf(", ");
 	    }
 	}
-    }
-        
+    }    
 }
 
+bool evaluation(void* matrix_a, void* matrix_b, int byte_len, int width, int height, float error) {
+    int num_elements = width * height;
+    assert(byte_len = 4); //suppose it's integer this version.
+    int num_error = 0;
+    for (int i = 0; i < num_elements; i++) {
+       if (*((int*)matrix_a + i) != *((int*)matrix_b+i)) {
+           return false;
+       }
+    }
+    return true;
 
+
+}
 
 int main() {
     float* matrix_a;
